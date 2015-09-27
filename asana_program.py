17ce85b97df1535659e8404111a777fe95367143
Chelsea Celine Little <cclittle@Chelseas-MacBook-Pro.local>
2015-09-26 17:41:50 -0700

    first save

---
+++ asana_program.py
@@ -0,0 +1,18 @@
+yoga_poses = {
+	"Integration":{
+	"childs_pose": 2,
+	"downwardfacingdog_pose": 2, 
+	"ragdoll_pose": 2}, 
+	"SunA": {
+	"plank": 1
+
+	}}
+
+print yoga_poses
+
+integration_question1 = raw_input ("Do you want to add integration?")
+
+print integration_question1
+
+
+
