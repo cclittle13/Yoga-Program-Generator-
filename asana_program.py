8dd33bbcdbb27b46f3c5066726deab6f16319a4a
Hackbright Student <info@hackbrightacademy.com>
2015-10-05 21:50:33 -0700

    add new material to files

--- asana_program.py
+++ asana_program.py
@@ -1,19 +1,58 @@
-yoga_poses = {
-	"Integration":{
-	"childs_pose": 2,
-	"downwardfacingdog_pose": 2, 
-	"ragdoll_pose": 2}, 
-	"SunA": {
-	"high to mid plank": 1,
-	"updog": 2
+import json
+with open("yoga_asanas_json.json") as json_data:
+    read_file = json_data.read()
+    for line in read_file:
+        print line
+# data= []
+# with open('yoga_asanas_json.json', 'r') as f:
+#     for line in f:
+#         data.append(json.loads(line))
+    #print f.read()
+    # for line in f:
+    #     print json.loads(line)
 
-	}}
-
-print yoga_poses
-
-integration_question1 = raw_input ("Do you want to add integration?")
-
-print integration_question1
+#print yoga_asanas_json
 
 
+
+# print yoga_asanas
+# integration_question1 = raw_input ("Do you want to add the Integration Series?")
+# print integration_question1
+ 
+# integration_question2 = raw_input ("Do you want to add the Intention Series?")
+# print integration_question2
+ 
+# integration_question3 = raw_input ("Do you want to add the Sun Salutation A Series?")
+# print integration_question3
+ 
+# integration_question4 = raw_input ("Do you want to add the Sun Salutation B Series?")
+# print integration_question4
+ 
+# integration_question5 = raw_input ("Do you want to add the Core Strengthening Series?")
+# print integration_question5
+ 
+# integration_question6 = raw_input ("Do you want to add the Crescent Lunge Series?")
+# print integration_question6
+ 
+# integration_question7 = raw_input ("Do you want to add the Balancing Series?")
+# print integration_question7
+ 
+# integration_question8 = raw_input ("Do you want to add the Triangle Series?")
+# print integration_question8
+ 
+# integration_question9 = raw_input ("Do you want to add the Hip Opener Series?")
+# print integration_question9
+ 
+# integration_question10 = raw_input ("Do you want to add the Spine Strengthening Series?")
+# print integration_question10
+ 
+# integration_question11 = raw_input ("Do you want to add the Forward Fold Series?")
+# print integration_question11
+ 
+# integration_question12 = raw_input ("Do you want to add the Surrender Series?")
+# print integration_question12
+ 
+# print integration_question1
+
+# formula to limit the time to between 60-90_min
 
