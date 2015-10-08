cc439615ee9b3469e7c66b1a28e77508f1974f81
Hackbright Student <info@hackbrightacademy.com>
2015-10-07 20:15:42 -0700

    created new file json to use- so you can use it later

--- asana_program.py
+++ asana_program.py
@@ -1,7 +1,7 @@
 import json
 with open("yoga_asanas_json.json") as json_data:
     read_file = json_data.read()
-    print read_file
+    #print read_file
 
 
 
@@ -18,14 +18,136 @@ with open("yoga_asanas_json.json") as json_data:
 
 
 # print yoga_asanas
-# integration_question1 = raw_input ("Do you want to add the Integration Series?")
-# print integration_question1
+
+# yoga_asanas = ["Integration", "Sun_Salutation A", "Sun Salutation B",  ]
+
+                                                                                                                      
+                                               
+                                                                                    
+# yoga_asanas = ["Integration", "Sun_Salutation A",   ]
+# yoga_asanas = ["Integration", "Sun_Salutation A", ]
+                        
+                             
+# integration_question1 = raw_input ("Do you want to add the Integration Series? ")
+
+# YES = bool(1)
+# FALSE = bool(0)
+
+integration_question1 = raw_input ("Do you want to add the Integration Series? ")
+
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"
+
+integration_question2 = raw_input ("Do you want to add the Intention Series?")
+#print integration_question2
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"
  
+integration_question3 = raw_input ("Do you want to add the Sun Salutation A Series?")
+#print integration_question3
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"                                                                                            
+ 
+integration_question4 = raw_input ("Do you want to add the Sun Salutation B Series?")
+#print integration_question4
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"                                                                                            
+
+ 
+integration_question5 = raw_input ("Do you want to add the Core Strengthening Series?")
+print integration_question5
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"                                                                                            
+
+ 
+integration_question6 = raw_input ("Do you want to add the Crescent Lunge Series?")
+print integration_question6
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"                                                                                            
+
+ 
+integration_question7 = raw_input ("Do you want to add the Balancing Series?")
+print integration_question7
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"                                                                                            
+
+ 
+integration_question8 = raw_input ("Do you want to add the Triangle Series?")
+print integration_question8
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"                                                                                            
+
+ 
+integration_question9 = raw_input ("Do you want to add the Hip Opener Series?")
+print integration_question9
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"                                                                                            
+
+ 
+integration_question10 = raw_input ("Do you want to add the Spine Strengthening Series?")
+print integration_question10
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"                                                                                            
+
+ 
+integration_question11 = raw_input ("Do you want to add the Forward Fold Series?")
+print integration_question11
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"                                                                                           
+integration_question12 = raw_input ("Do you want to add the Surrender Series?")
+print integration_question12
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"                                                                                            
+
+integration_question13 = raw_input ("Do you want to add Namaste?")
+print integration_question13
+if integration_question1 == "Yes":
+    print 'YES: 4_minutes'
+elif integration_question1 == "No":
+    print "NO: zero_minutes"                                                                                            
+
+# >>> x = int(raw_input("Please enter an integer: "))
+# Please enter an integer: 42000000000000000000000000000000000000000000000000
+# >>> if x < 0:0000000000000000
+# ...     x = 0
+# ...     print 'Negative changed to zero'
+# ... elif x == 0:
+# ...     print 'Zero'
+# ... elif x == 1:
+# ...     print 'Single'0000000000000000000000000000000000000000000000
+# ... else:
+# ...     print 'More'
+
+# print integration_question1
 # integration_question2 = raw_input ("Do you want to add the Intention Series?")
 # print integration_question2
  
 # integration_question3 = raw_input ("Do you want to add the Sun Salutation A Series?")
-# print integration_question3
+# print integration_question3                                                                                            
  
 # integration_question4 = raw_input ("Do you want to add the Sun Salutation B Series?")
 # print integration_question4
@@ -57,4 +179,6 @@ with open("yoga_asanas_json.json") as json_data:
 # print integration_question1
 
 # formula to limit the time to between 60-90_min
+
+
 
